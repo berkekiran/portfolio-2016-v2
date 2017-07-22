f7c28661477676672998bcb1f4e1012f54ca3ce0
Berke KIRAN <berkekiranofficial@hotmail.com>
2017-07-22 18:16:22 +0300

    update

--- index.php
+++ index.php
@@ -1,3 +1,51 @@
+<?php
+if(isset($_POST['submit'])):
+    if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
+		//your site secret key
+        $secret = '6LflESoUAAAAAFIh1iMOoYfi0T04AnHMoIuQ6Uqw';
+		//get verify response data
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+        $responseData = json_decode($verifyResponse);
+
+		$name = !empty($_POST['name'])?$_POST['name']:'';
+		$email = !empty($_POST['email'])?$_POST['email']:'';
+		$message = !empty($_POST['message'])?$_POST['message']:'';
+        if($responseData->success):
+			//contact form submission code
+			$to = 'berkekiranofficial2@gmail.com';
+			$subject = 'New contact form have been submitted';
+			$htmlContent = "
+				<h1>Contact request details</h1>
+				<p><b>Name: </b>".$name."</p>
+				<p><b>Email: </b>".$email."</p>
+				<p><b>Message: </b>".$message."</p>
+			";
+			// Always set content-type when sending HTML email
+			$headers = "MIME-Version: 1.0" . "\r\n";
+			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+			// More headers
+			$headers .= 'From:'.$name.' <'.$email.'>' . "\r\n";
+			//send email
+			@mail($to,$subject,$htmlContent,$headers);
+
+            $succMsg = 'Your contact request have submitted successfully.';
+			$name = '';
+			$email = '';
+			$message = '';
+        else:
+            $errMsg = 'Robot verification failed, please try again.';
+        endif;
+    else:
+        $errMsg = 'Please click on the reCAPTCHA box.';
+    endif;
+else:
+    $errMsg = '';
+    $succMsg = '';
+	$name = '';
+	$email = '';
+	$message = '';
+endif;
+?>
 <!DOCTYPE html>
 <html lang="en" ng-app="portfolio" ng-controller="projectController">
   <head>
@@ -15,7 +63,7 @@
     <meta name="theme-color" content="#0d1114">
     <!-- External -->
     <link type="text/css" rel="stylesheet" href="css/theme.css">
-    <script src="https://www.google.com/recaptcha/api.js"></script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.5/angular.min.js"></script>
     <script type="text/javascript" src="js/project.js"></script>
     <!-- Fonts -->
@@ -292,25 +340,26 @@
         <form action="" enctype="multipart/form-data" method="POST" accept-charset="ISO-8859-15">
           <span>Your Name (required)</span>
           <br>
-          <input class="classic" name="name" type="text" size="40" required placeholder="Name*">
+          <input class="classic" name="name" type="text" size="40" required placeholder="Name*" value="<?php echo !empty($name)?$name:''; ?>">
           <br>
           <span>Your Email (required)</span>
           <br>
-          <input class="classic" name="email" type="email" size="40" id="send_message" required placeholder="E-mail*">
+          <input class="classic" name="email" type="email" size="40" id="send_message" required placeholder="E-mail*" value="<?php echo !empty($email)?$email:''; ?>">
           <br>
           <span>Your Message</span>
           <br>
-          <textarea class="classic" name="message" cols="40" rows="5" placeholder="Message*"></textarea>
+          <textarea class="classic" name="message" cols="40" rows="5" placeholder="Message*"><?php echo !empty($message)?$message:''; ?></textarea>
           <br>
           <br>
           <div class="captcha_wrapper">
-            <div class="g-recaptcha" data-sitekey="6Lc9fSkUAAAAAFOvolMJtt_37YpuHIR0taP8PbNo">
-            </div>
+            <div class="g-recaptcha" data-theme="dark" data-sitekey="6LflESoUAAAAABpQ4V3nduP4sHvqeT3MN35HqJ6N"></div>
           </div>
           <br>
           <br>
           <input class="send-button classic" type="submit" name="submit" value="Send">
         </form>
+        <?php if(!empty($errMsg)): ?><div class="errMsg"><?php echo $errMsg; ?></div><?php endif; ?>
+        <?php if(!empty($succMsg)): ?><div class="succMsg"><?php echo $succMsg; ?></div><?php endif; ?>
       </section>
       <section class="footer classic">
         <ul class="classic">
